3ba269c678da33a8c326a6ecc0139c2c3b5157fb
Nicolás Rodríguez González <nicoassesin@gmail.com>
2022-04-06 14:08:00 +0200

    Primer commit

---
+++ src/edu/fpdual/ejemplo/project/Main.java
@@ -0,0 +1,15 @@
+package edu.fpdual.ejemplo.project;
+
+public class Main {
+
+    public static void main(String[] args) {
+	    new Main().printMessage();
+    }
+
+    public void printMessage() {
+
+        int variable = 5;
+
+        System.out.println(variable);
+    }
+}
